3e205da551ba0a77ba40f9e2d26b40621ae6e4cf
Sullivan Berger <sullivan.berger@gmail.com>
2024-09-19 17:01:16 +0200

    Feat: Try first markdown component

---
+++ components/content/InformationCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+type Props = {
+  title: string;
+  description?: string;
+  content: string;
+};
+
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>
+        {{ title }}
+      </CardTitle>
+      <CardDescription>
+        {{ description }}
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      {{ content }}
+    </CardContent>
+  </Card>
+</template>
